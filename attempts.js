96e25e67475c88d96ad919c90f500958184c0dd4
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-05-12 16:46:50 -0600

    Complementary DNA complete

--- attempts.js
+++ attempts.js
@@ -1,18 +1,39 @@
 // Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
 
+//
+// function cakes(recipe, available) {
+//   let need = Object.keys(recipe)
+//   let have = Object.keys(available)
+//   //break need into component parts so comparing each element will work
+//   function canMake(ing) {
+//     return need.filter(function(el) {
+//       return el.indexOf(ing) > -1;
+//     })
+//   }
+//   console.log(need);
+//   console.log(have);
+//   console.log(canMake(have));
+// }
+//
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
 
-function cakes(recipe, available) {
-  let need = Object.keys(recipe)
-  let have = Object.keys(available)
-  //break need into component parts so comparing each element will work
-  function canMake(ing) {
-    return need.filter(function(el) {
-      return el.indexOf(ing) > -1;
-    })
-  }
-  console.log(need);
-  console.log(have);
-  console.log(canMake(have));
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// DNAStrand ("ATTGC") # return "TAACG"
+//
+// DNAStrand ("GTAT") # return "CATA"
+
+const DNA = {
+  A: 'T', C: 'G', T: 'A', G: 'C'
 }
 
-cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+function DNAStrand(strand) {
+  let newDNA =
+  strand.split('').reduce(function(gene, letter) {
+    return gene + DNA[letter]
+  }, [])
+  console.log(newDNA);
+}
+
+DNAStrand('ATTGC')
